8d4f560cb74eaac58cfcdb34698c4064ef9675b7
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-19 02:01:51 +0600

    brand details page created with dynamic url

--- src/components/Home/Brands/Brand.jsx
+++ src/components/Home/Brands/Brand.jsx
@@ -1,14 +1,22 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Brand = ({ brand }) => {
   const { brand_name, brand_logo } = brand;
+  const brandLink = brand_name.toLowerCase();
   return (
-    <div className="border shadow-xl rounded-lg p-3  flex flex-col items-center justify-center gap-4">
-      <div className="flex-grow flex flex-col items-center justify-center">
-        <img className="w-[10rem] h-[10rem] rounded-lg " src={brand_logo} alt="" />
+    <Link to={`/brand/${brandLink}`} >
+      <div className="border shadow-xl rounded-lg p-3  flex flex-col items-center justify-center gap-4">
+        <div className="flex-grow flex flex-col items-center justify-center">
+          <img
+            className="w-[10rem] h-[10rem] rounded-lg "
+            src={brand_logo}
+            alt=""
+          />
+        </div>
+        <h3 className="text-2xl font-bold">{brand_name}</h3>
       </div>
-      <h3 className="text-2xl font-bold">{brand_name}</h3>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/BrandDetails.jsx
+++ src/pages/BrandDetails.jsx
@@ -1,6 +1,13 @@
+import { Helmet } from "react-helmet-async";
+import { useParams } from "react-router-dom";
+
 const BrandDetails = () => {
+  const { name } = useParams();
   return (
     <div>
+      <Helmet>
+        <title>Flavor Fusion | {name} </title>
+      </Helmet>
       <h3 className="text-4xl text-center font-semibold">
         Welcome to the Brand Details
       </h3>
